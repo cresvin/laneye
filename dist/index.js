7352afa4cdf3d5ee707ac41f56974fd34d233e9e
cresvin <oliwiertkacz2@gmail.com>
2024-09-02 07:41:26 +0200

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hello = (message) => {
+    console.log(message);
+};
+hello("hello world");
